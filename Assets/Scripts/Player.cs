1a5a8bae0f163b824b1957e433bf09b4cb46904b
l907793011 <35017088+l907793011@users.noreply.github.com>
2019-12-17 18:12:37 +0800

    角色
    添加角色预制件

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private int nSpeed = 1;
+    private bool bHInRun = false;  //水平位置是否处于移动状态
+    private bool bVInRun = false;  //垂直位置是否处于移动状态
+    private bool bRuning = false;  //是否处于移动状态
+    private Vector3 vDirection = Vector3.up; //移动的方向
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (h != 0 && v == 0)
+        {
+            Debug.Log("Move: " + h);
+            Vector3 vDir = Vector3.up;
+            if(h< 0)
+            {
+                vDir = Vector3.down;
+            }
+            if (vDirection != vDir)
+            {
+                vDirection = vDir;
+                ChangeDirection();
+
+            }
+        }
+        else if(v != 0){
+            Debug.Log("Move: " + v);
+        }
+    }
+
+    //向上
+    private void MoveUp()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * nSpeed, Space.World);
+    }
+
+    //向下
+    private void MoveDown()
+    {
+        transform.Translate(Vector3.down * Time.deltaTime * nSpeed, Space.World);
+    }
+
+    public void ChangeDirection()
+    {
+
     }
 }
