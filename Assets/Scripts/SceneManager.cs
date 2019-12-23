b790c8673242363820e3b63735583712adf16b38
l907793011 <35017088+l907793011@users.noreply.github.com>
2019-12-23 17:25:17 +0800

    json
    json读取修改

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using LitJson;
 using System.IO;
+using System;
 
 public class SceneManager : MonoBehaviour
 {
@@ -10,20 +11,27 @@ public class SceneManager : MonoBehaviour
 
     //private int nBarrier = 1;
 
+    private DataSecene dataSecene;
+       
     void Start()
     {
-        string strPath = Application.dataPath + "Resources/Static/map.json";
+        string strPath = Application.dataPath + "/Resources/Static/map.json";
         if (File.Exists(strPath))
         {
-            StreamReader sr = new StreamReader(strPath);
-            string jsonStr = sr.ReadToEnd();//获取json文件里面的字符串
-            sr.Close();
-            Debug.Log(jsonStr);
-            //for (int i = 0; i < jsonStr.Count; i++)//遍历获取数据
-            //{
+            //StreamReader sr = new StreamReader(strPath);
+            //string jsonStr = sr.ReadToEnd();//获取json文件里面的字符串
+            //sr.Close();
+            string strJson = File.ReadAllText(strPath);
+            Hashtable jd = JsonMapper.ToObject<Hashtable>(strJson);
+            JsonData jd1 = jd["map"] as JsonData;
+            for (int i = 0; i < jd1.Count; i++)
+            {
+                Debug.Log(jd1[i]["id"]);
+                Debug.Log(jd1[i]["map"]);
+            }
 
-            //}
-            //Save save = JsonMapper.ToObject<Save>(jsonStr); //将字符串转为save对象
+            //JsonData id = JsonMapper.ToObject(strJson);
+            //Debug.Log("id: " + id);
         }
     }
 
