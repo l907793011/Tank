286bb2c037735cc250024109ff9fff77b88bed05
l907793011 <35017088+l907793011@users.noreply.github.com>
2019-12-24 15:19:21 +0800

    场景
    场景创建

--- Assets/Scripts/DataSecene.cs
+++ Assets/Scripts/DataSecene.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 //[System.Serializable]
 public class DataSecene
@@ -18,12 +19,63 @@ public class DataSecene
     {
         
     }
+    public ObjBarrier GetBarrier(int n)
+    {
+        foreach (ObjBarrier obj in map)
+        {
+            if (obj.Index() == n)
+            {
+                return obj;
+            }
+        }
+        return null;
+    }
 }
 
 //[System.Serializable]
 public class ObjBarrier
 {
-    public int id;
+    public string id;
     public string map;
+
+    public int Index()
+    {
+        return Convert.ToInt32(id);
+    }
     //public List<int> map = new List<int>();
+    //获取关卡场景列表
+    public ArrayList GetBarrierList()
+    {
+        //Array[] arryScene = new Array[17];
+        //List<List<int>> lsScene = new List<List<int>>();
+
+        ArrayList list = new ArrayList();
+        string[] arry = map.Split('|');
+        //int nIndexX = 0;
+        foreach (string str in  arry)
+        {
+            // List<int> ls = new List<int>();
+
+            //int[] arMap = new int[21];
+            ArrayList alsMap = new ArrayList();
+            string[] ar = str.Split(';');
+            //int nIndexY = 0;
+            foreach (string strAr in ar)
+            {
+                int x = Convert.ToInt32(strAr);
+                alsMap.Add(x);
+                //Debug.Log(nIndexX + " : " + nIndexY + "   " + x);
+                //arMap[nIndexY] = x;
+                //nIndexY++;
+                //ls.Insert(x,x);
+            }
+            //arryScene[nIndexX] = arMap;
+            list.Add(alsMap);
+           // nIndexX++;
+
+            //lsScene.Insert(nIndex,ls);
+        }
+
+        return list;
+    }
 }
--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -10,28 +10,85 @@ public class SceneManager : MonoBehaviour
     //public SceneManager Instance;
 
     //private int nBarrier = 1;
+    public GameObject[] lsGo;
+    
+    private DataSecene dataScene;
+    private ArrayList alsBarrier;
 
-    private DataSecene dataSecene;
-       
+    //（-10,8）
+    public int nRowStart = -10; //行 起始点
+    public int nColStart = 8; //列 起始点
     void Start()
     {
         string strPath = Application.dataPath + "/Resources/Static/map.json";
         if (File.Exists(strPath))
         {
+            //1、
             //StreamReader sr = new StreamReader(strPath);
-            //string jsonStr = sr.ReadToEnd();//获取json文件里面的字符串
+            //string strJson = sr.ReadToEnd();//获取json文件里面的字符串
             //sr.Close();
-            string strJson = File.ReadAllText(strPath);
-            Hashtable jd = JsonMapper.ToObject<Hashtable>(strJson);
-            JsonData jd1 = jd["map"] as JsonData;
-            for (int i = 0; i < jd1.Count; i++)
-            {
-                Debug.Log(jd1[i]["id"]);
-                Debug.Log(jd1[i]["map"]);
-            }
 
+            //2、
+            //string strJson = File.ReadAllText(strPath);
+            //Hashtable jd = JsonMapper.ToObject<Hashtable>(strJson);
+            //JsonData jd1 = jd["map"] as JsonData;
+            //for (int i = 0; i < jd1.Count; i++)
+            //{
+            //    Debug.Log(jd1[i]["id"]);
+            //    Debug.Log(jd1[i]["map"]);
+            //}
+
+            //3、
+            StreamReader sr = new StreamReader(strPath);
+            string strJson = sr.ReadToEnd();//获取json文件里面的字符串
+            dataScene = JsonMapper.ToObject<DataSecene>(strJson);
             //JsonData id = JsonMapper.ToObject(strJson);
             //Debug.Log("id: " + id);
+            InitBarrier(1);
+        }
+    }
+
+    public void InitBarrier(int n)
+    {
+        ObjBarrier obj = dataScene.GetBarrier(n);
+        alsBarrier =  obj.GetBarrierList();
+        CreateGameObject(alsBarrier);
+    }
+
+    public void CreateGameObject(ArrayList alsBerrier)
+    {
+        GameObject objParent = GameObject.Find("objGame");
+        for (int i = 0; i < alsBerrier.Count; i++)
+        {
+            ArrayList alsRow =(ArrayList)alsBerrier[i];
+            for (int j = 0; j < alsRow.Count;j++ )
+            {
+                int nType = (int)alsRow[j];
+                GameObject obj = null;
+                switch (nType)
+                {
+                    case 1:
+                        obj = lsGo[0];
+                        break;
+                    case 2:
+                        obj = lsGo[1];
+                        break;
+                    case 3:
+                        obj = lsGo[2];
+                        break;
+                    case 4:
+                        obj = lsGo[3];
+                        break;
+                    default:
+                        break;
+                }
+                if (obj != null)
+                {
+                    GameObject go =  Instantiate(obj, objParent.transform);
+                    Vector3 vecPos = new Vector3(j - 10, 8 - i, 0);
+                    go.transform.position = vecPos;
+                }
+            }
         }
     }
 
