b8507c89075b3da64ae8bb4f272f5be2bc134fd9
l907793011 <35017088+l907793011@users.noreply.github.com>
2020-01-07 14:48:55 +0800

    【表格读取】添加表格读取文件

---
+++ Assets/Util/ApkReader.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+using ICSharpCode.SharpZipLib.Zip;
+using ICSharpCode.SharpZipLib.Core;
+//using UnityEditor.PackageManager;
+
+namespace Util
+{
+    public class ApkReader
+    {
+       // public delegate void OnCopyProgress(int curCount, int totalCount);
+
+        //public delegate void OnCopyFinish(Error error);
+
+        private ZipFile m_zipFile = null;
+        private Dictionary<string, long> m_nameIndex = new Dictionary<string, long>();
+        private Dictionary<string, string> m_pathCache = new Dictionary<string, string>();
+        private string m_assetsPath = "";
+
+        private static ApkReader s_apkReader = null;
+        public static ApkReader Instance
+        {
+            get
+            {
+                if (null == s_apkReader)
+                {
+                    s_apkReader = new ApkReader();
+                }
+                return s_apkReader;
+            }
+        }
+
+        private ApkReader()
+        {
+            DateTime t1 = DateTime.Now;
+            string apkPath = Application.dataPath;
+            m_assetsPath = apkPath + "/assets";
+            m_zipFile = new ZipFile(apkPath);
+            foreach (ZipEntry entry in m_zipFile)
+            {
+                if (entry.Name.IndexOf("assets/") != -1)
+                {
+                    m_nameIndex.Add(entry.Name, entry.ZipFileIndex);
+                }
+            }
+            DateTime t2 = DateTime.Now;
+            TimeSpan ts = t2.Subtract(t1).Duration();
+            Debug.LogFormat("apkreader {0}", ts.TotalSeconds);
+        }
+
+        public byte[] ReadAll(string pathOrName)
+        {
+            ZipEntry zipEntry = this.GetEntry(pathOrName);
+            if (null == zipEntry)
+                return null;
+            long size = zipEntry.Size;
+            byte[] allCtn = new byte[size];
+            Stream input = m_zipFile.GetInputStream(zipEntry.ZipFileIndex);
+            input.Read(allCtn, 0, (int)size);
+            input.Close();
+            return allCtn;
+        }
+
+        public ZipEntry GetEntry(string pathOrName)
+        {
+            string pathWithAssets = PathTransform(pathOrName);
+            long nameIndex = -1;
+            if (!m_nameIndex.TryGetValue(pathWithAssets, out nameIndex))
+            {
+                return null;
+            }
+
+            ZipEntry entry = m_zipFile[(int)nameIndex];
+            return entry;
+        }
+
+        protected string PathTransform(string pathOrName)
+        {
+            //Debug.LogFormat("PathTransform 91 {0}", pathOrName);
+            //jar:file:///data/app/com.daiei.gap-1.apk!/assets
+            string pathWithAssets = "";
+            if (m_pathCache.TryGetValue(pathOrName, out pathWithAssets))
+            {
+                return pathWithAssets;
+            }
+            string newPathOrName = pathOrName.Replace("\\", "/");
+            int index = newPathOrName.IndexOf(Application.streamingAssetsPath);
+            if (-1 == index)
+            {
+                pathWithAssets = "assets/" + newPathOrName;
+            }
+            else
+            {
+                int assestIndex = newPathOrName.IndexOf("/assets");
+                pathWithAssets = newPathOrName.Substring(assestIndex + 1);
+            }
+            //Debug.LogFormat("ApkReader PathTransform {0} platwithassets {1}", pathOrName, pathWithAssets);
+            m_pathCache[pathOrName] = pathWithAssets;
+            return pathWithAssets;
+        }
+
+        public bool Contains(string pathOrName)
+        {
+            string pathWithAssets = PathTransform(pathOrName);
+            bool hasContains = m_nameIndex.ContainsKey(pathWithAssets);
+            return hasContains;
+        }
+    }
+}
---
+++ Assets/Util/UtilFile.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using UnityEngine;
+
+namespace Util
+{
+    public class UtilFile
+    {
+        //搜索路径
+        private List<string> m_searchPathArray = new List<string>();
+
+        private static UtilFile instance;
+
+        public static UtilFile Instance{
+            get {
+                if (instance == null)
+                {
+                    instance = new UtilFile();
+                }
+                return instance;
+            }
+        }
+
+        protected void FileUtils()
+        {
+#if UNITY_EDITOR || UNITY_EDITOR_64 || UNITY_EDITOR_OSX 
+            m_searchPathArray.Add(Application.dataPath);
+#else
+            m_searchPathArray.Add(Application.streamingAssetsPath);
+#endif
+        }
+
+
+        public string  GetDataFromFile(string path)
+        {
+            StreamReader reader = new StreamReader(Application.persistentDataPath + path);
+            string jsonData = reader.ReadToEnd();
+            reader.Close();
+            reader.Dispose();
+            return jsonData;
+            //string json = "";
+            //TextAsset text = Resources.Load<TextAsset>(path);
+            //json = text.text;
+            ////if (string.IsNullOrEmpty(json)) return null;
+            //return json;
+
+            //string strPath = "";
+            //if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer ||
+            //    Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
+            //{
+            //    strPath = Application.dataPath + "/StreamingAssets/" + path;
+            //}
+            //else if (Application.platform == RuntimePlatform.IPhonePlayer)
+            //{
+            //    strPath = Application.dataPath + "/Raw/" + path;
+            //}
+            //else if (Application.platform == RuntimePlatform.Android)
+            //{
+            //    strPath = Application.persistentDataPath + path;
+            //    strPath = "jar:file://" + Application.dataPath + "!/assets/";
+            //    Debug.Log("GetDataFromFile  Android: " + strPath);
+            //    PritFullName(Application.persistentDataPath);
+            //}
+            //else
+            //{
+            //    strPath = Application.dataPath + "/config/" + path;
+            //}
+            //Debug.Log("GetDataFromFile  strPath: " + strPath);
+            //string strJson = "";
+            //if (File.Exists(strPath))
+            //{
+            //    Debug.Log("GetDataFromFile : " + "1111111111111111");
+
+
+            //    byte[] ctn = null;
+                //#if UNITY_EDITOR || UNITY_EDITOR_64 || UNITY_EDITOR_OSX || UNITY_IOS || UNITY_STANDALONE
+                //                ctn = File.ReadAllBytes(strPath);
+                //#elif UNITY_ANDROID
+                //            int indexOfAssets = newPath.IndexOf(Application.streamingAssetsPath) ;
+                //            if (indexOfAssets == 0)
+                //            {
+                //                ctn = ApkReader.Instance.ReadAll(newPath);
+                //            }
+                //            else
+                //            {
+                //                ctn = File.ReadAllBytes(strPath);
+                //            }  
+                //#endif
+                //                strJson = Encoding.ASCII.GetString(ctn);
+                //                Debug.Log("GetDataFromFile strJson : " + strJson);
+                //                2、
+                //            string strJson = File.ReadAllText(strPath);
+                //                Hashtable jd = JsonMapper.ToObject<Hashtable>(strJson);
+                //                JsonData jd1 = jd["map"] as JsonData;
+                //                for (int i = 0; i < jd1.Count; i++)
+                //                {
+                //                    Debug.Log(jd1[i]["id"]);
+                //                    Debug.Log(jd1[i]["map"]);
+                //                }
+
+                //                ctn = File.ReadAllBytes(newPath);
+                //                StreamReader sr = new StreamReader(strPath);
+                //                strJson = sr.ReadToEnd();//获取json文件里面的字符串
+                //            }
+                //            Debug.Log("GetDataFromFile  strJson: " + strJson);
+
+            //}
+//                Debug.Log("path: "+ path);
+//            string newPath = this.FullPathForFilename(path);
+//            Debug.Log("newPath: " + newPath);
+//            if (string.IsNullOrEmpty(newPath))
+//                return null;
+//            byte[] ctn = null;
+//#if UNITY_EDITOR || UNITY_EDITOR_64 || UNITY_EDITOR_OSX || UNITY_IOS || UNITY_STANDALONE
+//            Debug.Log("strJson: " + "3333333333333333333333333333");
+//            ctn = File.ReadAllBytes(newPath);
+//#elif UNITY_ANDROID
+//            int indexOfAssets = newPath.IndexOf(Application.streamingAssetsPath) ;
+//            if (indexOfAssets == 0)
+//            {
+//                Debug.Log("strJson: " + "1111111111111111111111111");
+//                ctn = ApkReader.Instance.ReadAll(newPath);
+//            }
+//            else
+//            {
+//                Debug.Log("strJson: " + "222222222222222222222222222");
+//                ctn = File.ReadAllBytes(newPath);
+//            }  
+//#endif
+//            Debug.Log("strJson 11111:  ");
+//            string strJson = strJson = Encoding.ASCII.GetString(ctn);
+//            Debug.Log("strJson 22222: " + strJson);
+            //return strJson;
+        }
+
+        private void PritFullName(string strPath)
+        {
+            foreach (string d in Directory.GetFileSystemEntries(strPath))
+            {
+                DirectoryInfo d1 = new DirectoryInfo(d);
+                Debug.Log(strPath + "  :  " + d1.Name);
+                PritFullName(d1.FullName);
+                //CleanDirectory(d1.FullName);////递归删除子文件夹
+                //Directory.Delete(d);
+            }
+        }
+
+
+        public string FullPathForFilename(string path)
+        {
+            if (path == null)
+                return "";
+
+            path = path.Replace("\\", "/");
+            //string outPath = "";
+            //if (m_cachedEntries.TryGetValue(path, out outPath))
+            //    return outPath;
+
+            bool isRoot = Path.IsPathRooted(path) || path.Contains(Application.streamingAssetsPath);
+            if (isRoot)
+            {
+                bool isExists = IsFileInSearchPath(path);
+                if (isExists)
+                {
+                    //m_cachedEntries[path] = path;
+                    return path;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+
+
+
+            for (int i = 0; i < m_searchPathArray.Count; i++)
+            {
+                string searchPath = m_searchPathArray[i];
+                string fullPath = Path.Combine(searchPath, path);
+                //Debug.LogFormat("FullPathForFilename 101 {0}", fullPath);
+                bool isExists = IsFileInSearchPath(fullPath);
+                if (isExists)
+                {
+                    //Debug.LogFormat("{0}", fullPath);
+                    //m_cachedEntries[path] = fullPath;
+                    return fullPath;
+                }
+            }
+
+            return "";
+        }
+
+        protected bool IsFileInSearchPath(string fullPath)
+        {
+            bool isExists = false;
+#if UNITY_EDITOR || UNITY_EDITOR_64 || UNITY_EDITOR_OSX || UNITY_IOS || UNITY_STANDALONE
+            isExists = File.Exists(fullPath);
+#elif UNITY_ANDROID
+            if(fullPath.IndexOf(Application.persistentDataPath) == 0)
+            {
+                isExists = File.Exists(fullPath); 
+            }
+            else
+            {
+                isExists = ApkReader.Instance.Contains(fullPath);
+            }
+#endif
+            return isExists;
+        }
+    }
+}
